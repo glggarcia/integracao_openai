895ccafcf9b400e9f6d2aba461e14b73b3c2fb01
gabrielgarcia <glggarcia2013@gmail.com>
2024-02-20 21:46:44 -0300

    Adiciona OpenAi service

---
+++ app/Http/Integrations/OpenAi/Plugins/OpenAiPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Integrations\OpenAi\Plugins;
+
+use Saloon\Http\PendingRequest;
+
+trait OpenAiPlugin
+{
+    public function bootOpenAiPlugin(PendingRequest $pendingRequest): void
+    {
+        //
+    }
+}
---
+++ app/Http/Integrations/OpenAi/Responses/GetOpenAIResponse.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Integrations\OpenAi\Responses;
+
+use Saloon\Http\Response;
+
+class GetOpenAIResponse extends Response
+{
+    //
+}
